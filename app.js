3e875323d0d78c42463cde72eb4dcc04af1067e0
Jitendra Gaur <jitendra.gaur@me.com>
2015-10-17 23:31:36 +0530

    Completed waiter tip calculator

---
+++ app.js
@@ -0,0 +1,48 @@
+angular.module('app', [])
+    .controller('mealController', function () {
+        vm = this;
+        vm.meal = {};
+
+        vm.mealInit = function () {
+            vm.meal_subtotal = 0,
+                vm.meal_tip = 0;
+            vm.meal_total = 0;
+
+            vm.meal_tipTotal = 0,
+                vm.meal_count = 0,
+                vm.meal_tipAvg = 0;
+        };
+
+        vm.mealInit();
+
+        vm.submitForm = function () {
+            //subTotal = (basemeal*tax/100)+basemeal
+            vm.meal_subtotal = (vm.meal.price * vm.meal.tax / 100) + vm.meal.price;
+            //tip = basemeal*tip/100
+            vm.meal_tip = vm.meal.price * vm.meal.tip / 100;
+            //total = subTotal + tip
+            vm.meal_total = vm.meal_subtotal + vm.meal_tip;
+
+            //tipTotal = mealTip + totalTip
+            vm.meal_tipTotal = vm.meal_tipTotal + vm.meal_tip;
+            //mealCount = mealCount++
+            vm.meal_count = vm.meal_count + 1;
+            //avgTip = totalTip/mealCount
+            vm.meal_tipAvg = vm.meal_tipTotal / vm.meal_tipTotal;
+
+            //reset the form
+            vm.resetForm();
+        };
+
+        vm.resetForm = function () {
+            vm.meal = {};
+            vm.theForm.$setPristine();
+        }
+
+        //reset whole app
+        vm.resetApp = function () {
+            vm.meal = {};
+            vm.theForm.$setPristine();
+            vm.mealInit();
+        }
+    })
